20655fa14016102c35063fefd8cb4f22540d5fc6
belter <onlybelter@outlook.com>
2024-03-20 13:55:21 +0800

    change files' name

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys
 from pathlib import Path
 HERE = Path(__file__).parent
 sys.path[:0] = [str(HERE.parent), str(HERE / 'extensions')]
-import biokowloonx
+# import biokowloon
 
 # sys.path.append(os.path.abspath(r'..'))
 
@@ -84,4 +84,3 @@ html_css_files = [
 source_suffix = ['.rst', '.md']
 
 fontawesome_included = True
-# print(biokowloon.sc.pub_func.__doc__)
